ef11569b1cae150814c1ac9329e5442039dcd441
Islam <68188384+islam-asankojoev@users.noreply.github.com>
2020-10-29 13:27:17 +0600

    asd

--- python.py
+++ python.py
@@ -1,15 +1,15 @@
+import re
 
+x = 1 + 2
+b = type(x)
 
-
-
-
-
-
-
-
+a = re.search('int|str|tuple|list|set',str(b))
+print('Эта функция выводит тип объекта')
 
 
 sqs = 0,1,4,9,16,25,36,49,64,81
 print(sqs[7:5:-1])
 
-x = 'asd'
+v = 'asd'
+
+print(v)

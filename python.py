3f2017cdcee39e17ba861d7c459363bd5b9715c8
Islam <68188384+islam-asankojoev@users.noreply.github.com>
2020-10-29 13:14:16 +0600

    asd

--- python.py
+++ python.py
@@ -1,13 +1,13 @@
+import re
+
+x = 1 + 2
+b = type(x)
+
+a = re.search('int|str|tuple|list|set',str(b))
+print('Эта функция выводит тип объекта')
 
 
-
-
-
-
-
-
-
-
+print(a.group(0))
 
 sqs = 0,1,4,9,16,25,36,49,64,81
 print(sqs[7:5:-1])
